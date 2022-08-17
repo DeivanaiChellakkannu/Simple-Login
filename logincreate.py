434af81758c3c406d2278ce6465ad90b019e949d
DeivanaiChellakkannu <111457600+DeivanaiChellakkannu@users.noreply.github.com>
2022-08-17 22:37:57 +0400

    Add files via upload

---
+++ logincreate.py
@@ -0,0 +1,11 @@
+print( 'Create a user now')
+username = input( 'Enter username : ')
+password=input('Enter password: ')
+print('User created successfully')
+print('Login now')
+username1=input('Enter username: ')
+password1=input('Enter password: ')
+if username== username1 and password==password1:
+    print('Logged in successfully')
+else:
+    print('Invalid credentials')
